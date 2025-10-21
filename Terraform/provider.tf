3b1b4d10a43002827bd763afffeb9cd87cf77d67
Raihan-chowdhury7 <raihan2cho@gmail.com>
2025-10-21 20:20:29 +0100

    Added s3 bucket to allow pipelin state and CI stae to match

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,11 +1,21 @@
 terraform {
   required_version = ">= 1.3.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "6.15.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "my-terraform-config-bucket-raihan"
+    key            = "state/devops-lab/terraform.tfstate"
+    region         = "eu-west-2"
+    encrypt        = true
+    dynamodb_table = "terraform-locks" 
+  }
+
 }
 
 provider "aws" {
